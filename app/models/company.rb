1483f21326502219755414b13b5770e6a3764b3c
johnnybigoo <jotalico@icloud.com>
2022-01-19 17:45:26 -0500

    Inserting models validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
   has_many :vacancies
+  validates :name, presence: true
+  validates :description, length: { minumum: 3, maximum: 1000 },
+            presence: true
 end
--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,11 @@
 class Vacancy < ApplicationRecord
   belongs_to :company
   mas_many :applicants
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :description, length: { minumum: 3, maximum: 1000 },
+            presence: true
+  validates :requirements, presence: true
+  validates :salary, presence: true
+  validates :available, presence: true
 end
